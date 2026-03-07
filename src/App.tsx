4a3ec0f41f0ba76cf94f4d040a9ebd8d952e7429
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-07 04:38:36 -0300

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
